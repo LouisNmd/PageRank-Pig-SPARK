86c276a582bfbe789c9cbac94b8ca8ed7840f099
Raphael TENAUD <raphael.tenaud@etu.univ-nantes.fr>
2021-10-20 21:55:55 +0200

    Added random worse/faster generation

--- pythonProject/src/main.py
+++ pythonProject/src/main.py
@@ -2,28 +2,53 @@ import csv
 import random
 import os
 
-if __name__ == "__main__":
+
+def goodGenCrawl(nbURL):
     home_path = os.path.dirname(os.path.abspath(__file__))
     f = open('crawl.csv', "w")
     csv_writer = csv.writer(f, delimiter="\t")
-    nbURL = 1000
     percentNetwork = 0.70
-    probDiffPerLoop = percentNetwork/nbURL
-    tabTargetUrl = ['']*nbURL
+    probDiffPerLoop = percentNetwork / nbURL
+    tabTargetUrl = [''] * nbURL
     for i in range(len(tabTargetUrl)):
         percentNetwork = 0.70
         for j in range(len(tabTargetUrl)):
             temp = random.random()
             if temp <= percentNetwork:
                 if tabTargetUrl[i] == '':
-                    tabTargetUrl[i] = '(x'+str(j)+')'
+                    tabTargetUrl[i] = '(x' + str(j) + ')'
                 else:
-                    tabTargetUrl[i] = tabTargetUrl[i] + ', (x'+str(j)+')'
+                    tabTargetUrl[i] = tabTargetUrl[i] + ', (x' + str(j) + ')'
             percentNetwork -= probDiffPerLoop
 
     for i in range(len(tabTargetUrl)):
-        baseUrl = 'x'+ str(i)
-        targetUrl = '{ '+ tabTargetUrl[i] + ' }'
+        baseUrl = 'x' + str(i)
+        targetUrl = '{ ' + tabTargetUrl[i] + ' }'
         row = [baseUrl, 1, targetUrl]
         csv_writer.writerow(row)
+
+
+def randomGenCrawl(nbURL):
+    home_path = os.path.dirname(os.path.abspath(__file__))
+    f = open('crawl.csv', "w")
+    csv_writer = csv.writer(f, delimiter="\t")
+    percentNetwork = 0.70
+    for i in range(nbURL):
+        baseUrl = 'x' + str(i)
+        targetUrl = ''
+        for j in range(nbURL):
+            temp = random.random()
+            if temp <= percentNetwork:
+                if targetUrl == '':
+                    targetUrl = '(x' + str(j) + ')'
+                else:
+                    targetUrl = targetUrl + ', (x' + str(j) + ')'
+        row = [baseUrl, 1, '{ '+targetUrl + ' }']
+        csv_writer.writerow(row)
+
+
+if __name__ == "__main__":
+    goodGenCrawl(10000)
+    #randomGenCrawl(10000)
+
 
